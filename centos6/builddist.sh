d272a1dddaa2219224634668ebaaa9ad5da85eb9
dkavanagh <dkavanagh@gmail.com>
2012-06-18 13:19:50 -0400

    updated euca repo to nightly till release repo is ready

--- centos6/builddist.sh
+++ centos6/builddist.sh
@@ -2,5 +2,5 @@
 #
 #
 #
-tar czf faststart.tgz README.md cloudvalidator.sh elrepo-release-*.elrepo.noarch.rpm epel-release-*.noarch.rpm euca-centos-*-x86_64.tgz euca2ools-release-2*el6.noarch.rpm eucalyptus-release-3.*.el6.noarch.rpm eucalyptus.conf eucalyptus3*.tgz fastinstall.sh imageinstall.sh
+tar czf faststart.tgz README.md cloudvalidator.sh elrepo-release-*.elrepo.noarch.rpm epel-release-*.noarch.rpm euca-centos-*-x86_64.tgz euca2ools-release-2*el6.noarch.rpm eucalyptus-release-nightly-3.*.el6.noarch.rpm eucalyptus.conf eucalyptus3*.tgz fastinstall.sh imageinstall.sh fs.pdf
 
